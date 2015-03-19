7bbca70d8e2bdd09aeff8347eeb04b68547c1acc
James Beckman <jsb.programming@gmail.com>
2015-03-19 16:15:13 -0700

    initial add

---
+++ Notepad.pro
@@ -0,0 +1,25 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-17T16:08:37
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Notepad
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        notepad.cpp
+
+HEADERS  += notepad.h
+
+FORMS    += notepad.ui
+
+DISTFILES +=
+
+RESOURCES += \
+    images.qrc
---
+++ notepad.cpp
@@ -0,0 +1,57 @@
+#include "notepad.h"
+#include "ui_notepad.h"
+
+#include <QFileDialog>
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+
+Notepad::Notepad(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
+
+void Notepad::on_quitButton_clicked()
+{
+    qApp->quit();
+}
+
+void Notepad::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QString(), tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could not open file %1: %2").arg(fileName).arg(file.errorString()));
+            return;
+        }
+        QTextStream in(&file);
+        ui->textEdit->setText(in.readAll());
+        file.close();
+    }
+}
+
+void Notepad::on_actionSave_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), QString(), tr("Text files (*.txt);;C++ Files (*.cpp *.h)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::WriteOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could not save file %1: %2").arg(fileName).arg(file.errorString()));
+        } else {
+            QTextStream out(&file);
+            out << ui->textEdit->toPlainText();
+            out.flush();
+            file.close();
+        }
+    }
+}
